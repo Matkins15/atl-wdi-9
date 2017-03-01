00ea5682cd3d0b3b7e81991c0480f2975c5f81e0
Matkins15 <matthew1atkins@gmail.com>
2017-02-28 21:57:18 -0500

    updated_lab_functions

--- unit_01/w01d04/student_labs/js-functions-lab/functions-lab-starter.js
+++ unit_01/w01d04/student_labs/js-functions-lab/functions-lab-starter.js
@@ -1,48 +1,82 @@
 // Question 1
-function maxOfTwoNumbers() {
-
-}
+function maxOfTwoNumbers(num1, num2) {
+	if(num1 > num2) {
+		return(num1);
+	} else {
+		return(num2);
+	} 
+} 
 
 // Question 2
-function maxOfThree() {
+function maxOfThree(num1, num2, num3) {
+	
+	if(num1 > num2 && num1 > num3) {
+		return(num1);
+	} 
+	
+	if(num2 > num3 && num2 > num1) {
+		return(num2); 
+	}
 
+	if(num3 > num2 && num3 > num1) {
+		return(num3);
+	}
 }
-
+	
 // Question 3
-function isCharacterAVowel() {
-
+function isCharacterAVowel(letters) {
+  if(letters === "a"|| letters === "e"|| letters === "i"|| letters === "o"|| letters === "u") {
+ console.log("true"); 
+} else {
+  console.log("false");
+}
 }
 
 // Question 4
-function sumArray() {
- 
-}
+function sumArray(array) {
+	startingDigit = 0;
+	for(var i=0; i < array.length; i++) {
+	  startingDigit = startingDigit + array[i];
+	} return startingDigit;
+} 
+var sumOfArray = [1,2,29,4,5];
 
 
 // Question 4
-function multiplyArray() {
-
+function multipleArray(array) {
+	startingDigit = 1;
+	for(var i=0; i < array.length; i++) {
+	  startingDigit = startingDigit*array[i];
+	} return startingDigit;
 }
-
+var productOfArray = [5,2,4,5];
 
 // Question 5
 var numberOfArguments = function(){
-  
+  return numberOfArguments.length;
 }
 
 
 
 // Question 6
-var reverseString = function (){
-  
+var reverseString = function (someString) {
+  var newString = '';
+for(var i=someString.length-1; i>= 0; i--) {
+  newString = newString + someString.charAt(i);
+  }
+  return newString;
 };
 
 
 // Question 7
-function findLongestWord () {
-  
+function findLongestWord (words) {
+    var emptyVariable = 0;
+    for(var i=0; i<words.length; i++) {
+      if(words[i].length > emptyVariable) {
+        emptyVariable = words[i].length;
+      }
+    } return emptyVariable;   
 }
-
 
 // Question 8
 function filterLongWords () {
