0b973a88af63af66937ea9273e0382d5d00ce78e
Matkins15 <matthew1atkins@gmail.com>
2017-03-01 22:11:41 -0500

    update homework

--- unit_01/w02d02/student_labs/in_class_click_events/script.js
+++ unit_01/w02d02/student_labs/in_class_click_events/script.js
@@ -1,3 +1,8 @@
 window.onload = function() {
   
+ var button = document.querySelector("button");
+
+button.addEventListener("click", function(event){
+  console.log("Executed in the callback function.");
+});
 }
