8771f92645e527ebdcae4a72461e8679979ec02e
Matkins15 <matthew1atkins@gmail.com>
2017-03-02 23:02:14 -0500

    homes_update

--- unit_01/w02d04/instructor_notes/homes/script.js
+++ unit_01/w02d04/instructor_notes/homes/script.js
@@ -4,54 +4,44 @@ $(function() {
   console.log('Everything is ready!');
 
   $('#addHome').removeClass('btn-danger').addClass('btn-success');
-  $('.jumbotron').addClass('text-center');
 
-  var $newLink = $('<br><br><a id="zillowLink" href="http://www.zillow.com">Visit Zillow</a>')
+  var newLink = $( '<br><br><a id="zillowLink" href="http://www.zillow.com">Visit Zillow.com<a>' );
+  $('body').append(newLink);
 
-  $newLink.appendTo('body')
+});
 
-  $('#zillowLink').attr('target', '_blank');
+//$('#addHome').on('click', function(home) {
+  //  console.log(home)
+  //});
 
-  $('#addHome').click(function($event) {
-    console.log($event);
-    console.log(this)
-  })
 
-  var removeHome = function($event) {
-    console.log($event.target)
-    console.log(this)
-    if($($event.target).is('button')) {
-      $(this).fadeOut(1000, function() {
-        $(this).remove()
-      })
+$('#homes tbody').on('click', 'button', function () {
+  alert("You are about to delete this row.");
+  });
+
+var index = 0;
+$('#addHome').on('click', function() {
+    var home = newHomes[index];
+    var thisHouse = "<tr><td>" + home.address + "</td>";
+    console.log(thisHouse);
+    var thisSf = "<td>" + home.sf + "</td>";
+    console.log(thisSf);
+    var thisBedroom = "<td>" + home.bedrooms + "</td>";
+    var thisBath = "<td>" + home.baths + "</td>";
+    var thisPrice = "<td>" + home.price + "</td>";
+    var button = '<td><button class="btn btn-xs btn-danger">Remove</button></td></tr>';
+    var homeCompleted = thisHouse + thisSf + thisBedroom + thisBath + thisPrice + button;
+    $('tbody').append(homeCompleted);
+    index++;
+    if (index.length > 4) {
+      alert("no more homes to vie");
     }
-  }
+  });
 
-  $('#homes tbody').on('click', 'tr', removeHome)
-
-  $('#addHome').click(function($event) {
-    var home = newHomes.pop();
-
-    var $homeTr = $(
-      `
-      <tr>
-        <td>$()</td>
-        <td>${home.sf}</td>
-        <td>${home.bedrooms}</td>
-        <td>${home.baths}</td>
-        <td>${home.price}</td>
-        <td><button class="btn btn-xs btn-danger">Remove</button></td>
-      </tr>
-      `
-    );
-
-    $homeTr.appendTo('tbody');
-  })
-
-  var newHomes = [
-      {address: "27569 Cedarwood Drive", sf: "2,535", bedrooms: 3, baths: 2.5, price: "$496,500"},
-      {address: "316 Annandale Drive", sf: "1,326", bedrooms: 4, baths: 2, price: "$275,000"},
-      {address: "251 Grandview Road", sf: "3,800", bedrooms: 3, baths: 2, price: "$699,900"},
-      {address: "28571 Manitoba", sf: "2,960", bedrooms: 4, baths: 3.5, price: "$775,000"}
-  ];
-})
+var newHomes = [
+    {address: "27569 Cedarwood Drive", sf: "2,535", bedrooms: 3, baths: 2.5, price: "$496,500"},
+    {address: "316 Annandale Drive", sf: "1,326", bedrooms: 4, baths: 2, price: "$275,000"},
+    {address: "251 Grandview Road", sf: "3,800", bedrooms: 3, baths: 2, price: "$699,900"},
+    {address: "28571 Manitoba", sf: "2,960", bedrooms: 4, baths: 3.5, price: "$775,000"}
+]
+// });
