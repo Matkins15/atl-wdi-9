03fcedd63f4ed23c2a6a26d34e3b5eca9d1b9de3
Matkins15 <matthew1atkins@gmail.com>
2017-03-04 17:32:54 -0500

    stop watch is fully functional

--- unit_01/w02d03/homework/hw.js
+++ unit_01/w02d03/homework/hw.js
@@ -1,70 +1,101 @@
 // General Assembly, WDI (Web Development Immersive) Remote, Cohort 02 (R2D2)
 // Copyright (C) 2016 Matt Brendzel under the GNU General Public License.
 // See LICENSE for details.
-
 /// Data & Core Business Logic ///
 const Stopwatch = {
-  tickClock: function(){
-    if (Stopwatch.isRunning) {
-      setTimeout(Stopwatch.tickClock, 10); // trigger next clock tick
-      Stopwatch.advanceTenMillisecs();
-      AppController.handleClockTick();
-    }
-  },
-  isRunning: false,
-  mins: 0,
-  secs: 0,
-  millisecs: 0,
-  laps: [],
-  // DO NOT EDIT ABOVE THIS LINE
-  advanceTenMillisecs: function(){
-    // Your Code Here
-  },
-  reset: function(){
-    // Your Code Here
-  },
-  start: function(){
-    // Your Code Here
-  },
-  stop: function(){
-    // Your Code Here
-  },
-  lap: function(){
-    // Your Code Here
-  }
+ tickClock: function(){
+   if (Stopwatch.isRunning) {
+     setTimeout(Stopwatch.tickClock, 10); // trigger next clock tick
+     Stopwatch.advanceTenMillisecs();
+     AppController.handleClockTick();
+   }
+ },
+ isRunning: false,
+ mins: 0,
+ secs: 0,
+ millisecs: 0,
+ laps: [],
+ // DO NOT EDIT ABOVE THIS LINE
+ advanceTenMillisecs: function(){
+   // Your Code Here
+   Stopwatch.millisecs += 10;
+   ViewEngine.updateTimeDisplay();
+   if(Stopwatch.millisecs === 1000) {
+    Stopwatch.millisecs = 0;
+    Stopwatch.secs += 1;
+   }
+   if(Stopwatch.secs === 60){
+     Stopwatch.secs = 0;
+     Stopwatch.mins += 1;
+   }
+ },
+ reset: function(){
+  Stopwatch.mins = 0;
+  Stopwatch.secs = 0;
+  Stopwatch.millisecs = 0;
+  ViewEngine.updateTimeDisplay();
+},
+ start: function(){
+   Stopwatch.isRunning = true;
+   Stopwatch.tickClock();
+ },
+ stop: function(){
+   Stopwatch.isRunning = false;
+},
+ lap: function(){
+   Stopwatch.laps.push(document.getElementById("time-display").innerHTML);
+   ViewEngine.updateLapListDisplay();
+ }
 };
-
 /// User Interface ///
 const ViewEngine = {
-  updateTimeDisplay: function(mins, secs, millisecs){
-    // Your Code Here
-  },
-  updateLapListDisplay: function(laps){
-    // Your Code Here
-  },
+ updateTimeDisplay: function(mins, secs, millisecs){
+   // Your Code Here
+   $('#millisecs').text(Stopwatch.millisecs);
+   $('#secs').text(Stopwatch.secs);
+   $('#mins').text(Stopwatch.mins);
+ },
+ updateLapListDisplay: function(laps){
+   $("#lap-list").append( "<li>" + Stopwatch.laps[Stopwatch.laps.length -1] + "</li>")
+ },
 };
 const ViewHelpers = {
-  zeroFill: function(number, length){
-    // Your Code Here
-  },
+ zeroFill: function(number, length){
+   // Your Code Here
+ },
 };
-
 /// Top-Level Application Code ///
 const AppController = {
-  handleClockTick: function(){
-    // Your Code Here
-  },
-  handleClickStart: function() {
-    // Your Code Here
-  },
-  handleClickStopReset: function(){
-    // Your Code Here
-  },
-  handleClickLap: function(){
-    // Your Code Here
+ handleClockTick: function(){
+   console.log('send time to UI');
+ },
+ handleClickStart: function() {
+   console.log('handleClickStart fired');
+   if (Stopwatch.isRunning === true) {
+     console.log('stopWatch is currently running');
+   }
+   else {
+     Stopwatch.start();
+     console.log('stopWatch is not currently running');
+   }
+ },
+ handleClickStopReset: function(){
+  if (Stopwatch.isRunning === true) {
+      Stopwatch.stop();
+  } else {
+    Stopwatch.reset();
   }
+ },
+ handleClickLap: function(){
+   if(Stopwatch.isRunning === true){
+     Stopwatch.lap();
+   } else {
+       console.log('handleClickLap fired');
+   }
+ }
 };
-
 window.onload = function(){
-  // Attach AppController methods to the DOM as event handlers here.
+ $('#start').on('click', AppController.handleClickStart);
+ $('#lap').on('click', AppController.handleClickLap);
+ $('#stop').on('click', AppController.handleClickStopReset);
 };
