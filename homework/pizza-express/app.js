950d71f6fcd4c44cc7e145f1b12aa691d03a6697
Matkins15 <matthew1atkins@gmail.com>
2017-03-14 18:25:27 -0400

    completed GET routes

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -13,3 +13,16 @@ app.listen(port, function(){
   console.log('LISTENING ON PORT ' + port);
   console.log("==========================")
 });
+
+app.get('/', function(req, res) {
+  res.send("Welcome to Pizza Express");
+});
+
+app.get('/topping/:type', function(req, res, next) {
+    res.send(`${req.params.type} pizza! Good choice.`);
+});
+
+app.get('/order/:amount/:size', function(req, res, next) {
+
+   res.send(`Your order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`);
+});
