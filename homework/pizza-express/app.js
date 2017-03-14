625de4dd7017db3d31c87442c63eaefd25f40c92
Matkins15 <matthew1atkins@gmail.com>
2017-03-14 19:12:55 -0400

    Pizza Express

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -7,6 +7,13 @@ var app     = express();
 // assigning 3000 as our port
 var port    = 3000;
 
+var hbs = require('hbs');
+
+app.use(express.static(__dirname + '/public')); // VERY IMPORTANT!! Make sure to add a '/'
+
+app.set("view engine", "hbs");
+app.set('views', './views');
+
 // tells the server to listen for requests on port 3000
 app.listen(port, function(){
   console.log("==========================")
@@ -15,14 +22,22 @@ app.listen(port, function(){
 });
 
 app.get('/', function(req, res) {
-  res.send("Welcome to Pizza Express");
+  res.render("index.hbs", {
+});
+  // res.send("Welcome to Pizza Express");
 });
 
 app.get('/topping/:type', function(req, res, next) {
-    res.send(`${req.params.type} pizza! Good choice.`);
+  res.render('toppings.hbs', {
+    type: req.params.type
+  });
+    // res.send(`${req.params.type} pizza! Good choice.`);
 });
 
 app.get('/order/:amount/:size', function(req, res, next) {
-
-   res.send(`Your order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`);
+  res.render('order.hbs', {
+    amount: req.params.amount,
+    size: req.params.size
+  });
+  //  res.send(`Your order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`);
 });
