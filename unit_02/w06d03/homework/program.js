d3285678a53e04f992dd8de56d308e56a982b1c0
Matkins15 <matthew1atkins@gmail.com>
2017-03-15 18:52:46 -0400

    Completed Exercises

--- unit_02/w06d03/homework/program.js
+++ unit_02/w06d03/homework/program.js
@@ -18,8 +18,31 @@ app.use(bodyparser.urlencoded({extended: false}))
   // ----------------------------------------------
   // GOOD OLD FORM - Exercise 3
 
-  app.post('/form', function(req, res){
-    req.body.str.split('').reverse().join('');
-  });
+  // app.post('/form', function(req, res){
+  //   res.send(req.body.str.split('').reverse().join(''));
+  // });
 
-  app.listen(process.argv[2])
+  // ----------------------------------------------
+  // PARAMS PAM PAM - Exercise 4
+
+      // app.put('/message/:id', function(req, res) {
+      //       var id = req.params.id;
+      //       var str = require('crypto')
+      //       .createHash('sha1')
+      //       .update(new Date().toDateString() + id)
+      //       .digest('hex')
+      //
+      //       res.send(str);
+      //     })
+      //
+
+  // -----------------------------------------------
+  //WHATS IN QUERY - Lesson 7
+
+    app.get('/search', function(req, res) {
+        var query = req.query;
+            res.send(query);
+        })
+
+    app.listen(process.argv[2])
+  // -----------------------------------------------
