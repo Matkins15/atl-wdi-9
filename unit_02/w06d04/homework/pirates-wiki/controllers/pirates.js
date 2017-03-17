cec6fe0024a674bd989b93a847e5ab2a7341787e
Matkins15 <matthew1atkins@gmail.com>
2017-03-16 22:19:32 -0400

    Completed Pirates Homework

--- unit_02/w06d04/homework/pirates-wiki/controllers/pirates.js
+++ unit_02/w06d04/homework/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,44 @@
-//your code below
+//requirements
+var express = require("express");
+var router = express.Router();
+var pirates = require('../models/pirates.js');
+
+//index route
+router.get('/', function(req, res) {
+    res.render('pirates/index', {
+      pirates: pirates.famousPirates
+    });
+});
+
+/* CREATE TODO */
+router.post('/', function(req, res){
+    var newPirate = {
+        name: req.body.name,
+        birthplace: req.body.birthplace,
+        death_year: req.body.death_year,
+        base: req.body.base,
+        nickname: req.body.birthplace
+    };
+    console.log(newPirate);
+    pirates.famousPirates.push(newPirate);
+    res.redirect('/pirates');
+});
+
+//new route //why do we place the new route above the show route?
+router.get('/new', function(req, res){
+    res.render('pirates/new');
+  });
+
+//show route
+router.get('/:id', function(req, res){
+    var whichPirate = pirates.famousPirates
+
+    res.render('pirates/show', {
+      whichPirate: whichPirate
+  });
+});
+
+
+
+//exports
+module.exports = router;
--- unit_02/w06d04/homework/pirates-wiki/server.js
+++ unit_02/w06d04/homework/pirates-wiki/server.js
@@ -1,1 +1,19 @@
-//your code here
+var path = require('path');
+var express = require('express');
+var hbs = require('hbs');
+var app = express();
+
+var port = process.env.PORT || 3000;
+//controllers for `/pirates` resource
+var pirateController = require('./controllers/pirates.js');
+var bodyParser = require('body-parser');
+
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.use("/pirates", pirateController);
+
+app.listen(3000);
