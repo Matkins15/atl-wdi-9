3826177d1e743985ff3c3bc270f037d9fc138135
Matkins15 <matthew1atkins@gmail.com>
2017-03-20 17:59:06 -0400

    added two tamagotchis

--- unit_02/w07d02/homework/tamagotchi.js
+++ unit_02/w07d02/homework/tamagotchi.js
@@ -1,9 +1,27 @@
 console.log('tamagotchi file is loaded');
 
 //your constructor function here
+function Tamagotchi() {
+  this.foodInTummy = 10;
+  this.restedness = 10;
+  this.health = 10;
+
+  this.cry = function(){
+    var smallerTummy = this.foodInTummy - 1;
+    console.log(smallerTummy);
+    console.log("WAHH!!!");
+  };
+};
 
 
 //create new Tamagotchis
+var tamagotchiMatt = new Tamagotchi();
+var tamagotchiAylin = new Tamagotchi();
 
+console.log(tamagotchiMatt);
+console.log(tamagotchiAylin);
+
+tamagotchiMatt.cry();
+tamagotchiAylin.cry();
 
 //test out your Tamagotchies below via console.logs
