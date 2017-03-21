68a82aa9925ed232c33482259ece39fdb131abab
Matkins15 <matthew1atkins@gmail.com>
2017-03-20 23:39:27 -0400

    MVP

--- unit_02/w07d02/homework/tamagotchi.js
+++ unit_02/w07d02/homework/tamagotchi.js
@@ -17,7 +17,7 @@ function Tamagotchi(name, creatureType) {
         this.cry = setInterval(function(){
           console.log(`${this.name}`); }, 6000);
     }; hungerTimer();
-  };
+};
 
   this.puke = function(){
     this.smallerTummy = this.foodInTummy - 1;
@@ -27,31 +27,33 @@ function Tamagotchi(name, creatureType) {
         this.puke = setInterval(function(){
           console.log(`${this.name}`); }, 10000);
     }; sickTimer();
-  };
+};
 
   this.yawn = function(){
     this.nosleep = this.restedness - 1;
     console.log(`${this.name} has a current restedness of: ${this.nosleep}`);
 
     function yawnTimer() {
-        this.puke = setInterval(function(){
+        this.yawn = setInterval(function(){
           console.log(`${this.name}`); }, 20000);
     }; yawnTimer();
-  };
-
 };
 
+
+  // var StopTimer = function() {
+  //   this.yawn = yawnTimer;
+  //   this.puke = sickTimer;
+  //   this.cry = hungerTimer;
+  //
+  //   clearInterval(hungerTimer);
+  //   clearInterval(sickTimer);
+  //   clearInterval(yawnTimer);
+  // };
+
+};
 
 //create new Tamagotchis
 var tamagotchi1 = new Tamagotchi("Matt", "Beast");
 var tamagotchi2 = new Tamagotchi("Lin", "Flower");
 var tamagotchi3 = new Tamagotchi("Harry", "Joker");
 var tamagotchi4 = new Tamagotchi("Ray", "Lasagna");
-
-
-//test out your Tamagotchies below via console.logs
-// var myVar = this.cry;
-//
-// function myStartFunction() {
-//     this.cry = setInterval(function(){ console.log(`${this.name}`); }, 2000);
-// }
